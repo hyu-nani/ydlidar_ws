2b575a73cc99d2432f61dd7b8effc9a09310c4a3
hyu-nani <cube_h2@naver.com>
2021-11-15 13:59:44 +0900

    Update ydlidar_node.cpp

--- src/ydlidar_ros/src/ydlidar_node.cpp
+++ src/ydlidar_ros/src/ydlidar_node.cpp
@@ -225,21 +225,11 @@ int main(int argc, char * argv[]) {
             
         }
 	/////////////////////////////////////////////////////////////////////
-	int number = 1 ;
+	int number = 5 ;
 	data_average = (data_average * (number-1) + YD_distance[252])/number ;
-    	
     if(data_average < 0.4){	//trans MS "D13/1"
 		SerialPrint("10 0 0"); //X Y angle
-		/*
-		fd = open("/dev/ttyACM0", O_RDWR | O_NOCTTY | O_NDELAY);	//detect USB for arduino
-		serial1 = write(fd,"10 0 0",6);
-		if(serial1 < 0){
-			perror("write failed - ");
-		}
-		close(fd);
-		*/
 	}
-	
 	///////////////////////////////////////////////////////////////////////////read
 	SerialRead();
   	//////////////////////////////////////////////////////////////////////////
