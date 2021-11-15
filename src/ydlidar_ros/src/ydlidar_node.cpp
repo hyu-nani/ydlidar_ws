cbee5ed4eef9b38ced7f3460f6beb699feccf228
hyu-nani <cube_h2@naver.com>
2021-11-15 13:50:27 +0900

    Update ydlidar_node.cpp

--- src/ydlidar_ros/src/ydlidar_node.cpp
+++ src/ydlidar_ros/src/ydlidar_node.cpp
@@ -32,7 +32,7 @@ bool init1 = true;
 float data_average=0;
 
 void SerialPrint(char* strBuffer);
-void SerialRead(char* strBuffer)
+void SerialRead(char* strBuffer);
 
 std::vector<float> split(const std::string &s, char delim) {
     std::vector<float> elems;
