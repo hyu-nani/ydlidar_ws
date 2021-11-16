5fd3fc807d0ebf7aee442be0f1dcf7115d071fcb
hyu-nani <cube_h2@naver.com>
2021-11-17 01:39:42 +0900

    Update ydlidar_node.cpp

--- src/ydlidar_ros/src/ydlidar_node.cpp
+++ src/ydlidar_ros/src/ydlidar_node.cpp
@@ -244,7 +244,7 @@ int main(int argc, char * argv[]) {
 			}
 		}
 		if(active == true){
-			printf("%d\tScan! Angle:%f\n",printCount,angleNum);
+			printf("%d\tScan! Angle:%f\n",printCount,YD_angle[angleNum]);
 			printCount++;
 		}
 		if(data_average[252] < 0.4){	//trans MS
