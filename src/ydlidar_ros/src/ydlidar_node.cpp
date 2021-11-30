147fb348ac6fdab6850a5b8d27cda03a9e625e99
hyu-nani <cube_h2@naver.com>
2021-12-01 00:23:30 +0900

    Update ydlidar_node.cpp

--- src/ydlidar_ros/src/ydlidar_node.cpp
+++ src/ydlidar_ros/src/ydlidar_node.cpp
@@ -65,7 +65,9 @@ void scanCallback(const sensor_msgs::LaserScan::ConstPtr& scan)
 	for(int i = 0; i < lidarReadCount; i++) {
         float degree = RAD2DEG(scan->angle_min + scan->angle_increment * i);
         YD_angle[i] = degree;
-		YD_distance[i] = scan->ranges[i];
+        float val = scan->ranges[i];
+        if(val != 0)
+        	YD_distance[i] = val;
 		if(YD_angle[i]> -5 && YD_angle[i]< 5){
         	printf("angle-distance[%f - %f]%d\n",YD_angle[i],YD_distance[i],i);
    		}
