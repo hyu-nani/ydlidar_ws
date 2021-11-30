3fec6446f6f02ff0e83463bc5878231e154da728
hyu-nani <cube_h2@naver.com>
2021-12-01 03:46:13 +0900

    Update ydlidar_node.cpp

--- src/ydlidar_ros/src/ydlidar_node.cpp
+++ src/ydlidar_ros/src/ydlidar_node.cpp
@@ -335,10 +335,10 @@ void printSSHmonitor(int currentY,int currentX){
 	pinMap[printSize/2][printSize/2] = 3;
 	printf("\n");
 	
-	for(int i=0;i<printSize/2-2;i++)
+	for(int i=0;i<printSize/2-1;i++)
 	printf("--");
 	printf("FRONT");
-	for(int i=0;i<printSize/2-2;i++)
+	for(int i=0;i<printSize/2-1;i++)
 	printf("--");
 	printf("\n");
 	for(int i=0;i<printSize;i++){
@@ -357,10 +357,10 @@ void printSSHmonitor(int currentY,int currentX){
 		}
 		printf("|\n");
 	}
-	for(int i=0;i<printSize/2-2;i++)
+	for(int i=0;i<printSize/2;i++)
 	printf("--");
 	printf("BACK");
-	for(int i=0;i<printSize/2-2;i++)
+	for(int i=0;i<printSize/2-1;i++)
 	printf("--");
 	printf("\n");
 }
