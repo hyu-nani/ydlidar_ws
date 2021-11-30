8bc41c9841aaac537956c61f3a3170e6375efca6
hyu-nani <cube_h2@naver.com>
2021-12-01 01:30:56 +0900

    Update ydlidar_node.cpp

--- src/ydlidar_ros/src/ydlidar_node.cpp
+++ src/ydlidar_ros/src/ydlidar_node.cpp
@@ -247,9 +247,9 @@ int main(int argc, char * argv[]) {
 			else{
 				data_count[i] = 0;
 			}
-			if(data_count[i] > 10){
-				int Xvalue = sin(YD_angle[i])*YD_distance[i]*1;
-				int Yvalue = cos(YD_angle[i])*YD_distance[i]*1;
+			if(data_count[i] > 30){
+				int Xvalue = sin(YD_angle[i])*YD_distance[i]*10;
+				int Yvalue = cos(YD_angle[i])*YD_distance[i]*10;
 				allMap[robotY+Yvalue][robotX+Xvalue] = 1;
 				data_count[i] = 0;
 				active = true;
