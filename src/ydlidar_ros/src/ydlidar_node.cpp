bd3d96ba57000e5243326d131745968d41305a17
hyu-nani <cube_h2@naver.com>
2021-12-01 11:53:18 +0900

    Update ydlidar_node.cpp

--- src/ydlidar_ros/src/ydlidar_node.cpp
+++ src/ydlidar_ros/src/ydlidar_node.cpp
@@ -40,7 +40,7 @@ int		lidarReadCount;
 const int	printSize						=	50;
 int			pinMap[printSize][printSize]	=	{0};
 	
-float		unitScale	=	5.0;//1unit cm
+float		unitScale	=	8.0;//1unit cm
 const int	allMapSize = 10000;
 int		allMap[allMapSize][allMapSize] = {0};			//All map
 int		robotX = allMapSize/2, robotY = allMapSize/2;	//center
