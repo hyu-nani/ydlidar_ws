6383bcd48ee7c5f09cfccceafb069ecb667e9a3f
hyu-nani <cube_h2@naver.com>
2021-12-01 11:42:09 +0900

    Update ydlidar_node.cpp

--- src/ydlidar_ros/src/ydlidar_node.cpp
+++ src/ydlidar_ros/src/ydlidar_node.cpp
@@ -39,7 +39,7 @@ int		lidarReadCount;
 
 const int	printSize						=	50;
 int			pinMap[printSize][printSize]	=	{0};
-int			unitDistance	=	10;//cm
+int			unitScale	=	2;//1unit cm
 const int	allMapSize = 10000;
 int		allMap[allMapSize][allMapSize] = {0};			//All map
 int		robotX = allMapSize/2, robotY = allMapSize/2;	//center
@@ -241,8 +241,8 @@ int main(int argc, char * argv[]) {
 		//////////////////////////////////////////////////////////////////////////
 		for(int i=0;i<500;i++){
 			float difference = fabs(old_distance[i] - YD_distance[i]);
-			int Xvalue = round(-sin(YD_angle[i]*M_PI/180.0)*YD_distance[i]*100/unitDistance);
-			int Yvalue = round(-cos(YD_angle[i]*M_PI/180.0)*YD_distance[i]*100/unitDistance);
+			int Xvalue = round(-sin(YD_angle[i]*M_PI/180.0)*YD_distance[i]*100/unitScale);
+			int Yvalue = round(-cos(YD_angle[i]*M_PI/180.0)*YD_distance[i]*100/unitScale);
 			if( (difference < 0.01) &&	(difference != 0) && (YD_distance[i] > 0.15)){
 				data_count[i]++;
 				if(allMap[robotY+Yvalue][robotX+Xvalue]==0){
