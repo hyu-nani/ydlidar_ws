df7ec4a77c7aa826008eff4abf526c3496aff200
hyu-nani <cube_h2@naver.com>
2021-12-03 20:05:34 +0900

    Update ydlidar_node.cpp

--- src/ydlidar_ros/src/ydlidar_node.cpp
+++ src/ydlidar_ros/src/ydlidar_node.cpp
@@ -253,7 +253,7 @@ int main(int argc, char * argv[]) {
 			int Yvalue = round(-cos(YD_angle[i]*M_PI/180.0)*YD_distance[i]*100.0/unitScale);
 			if(allMap[robotY+Yvalue][robotX+Xvalue] < 2)
 				allMap[robotY+Yvalue][robotX+Xvalue] = 1; //sense
-			if( (difference < 0.02) &&	(difference != 0) && (YD_distance[i] > 0.15))
+			if( (difference < 0.01) &&	(difference != 0) && (YD_distance[i] > 0.15))
 				data_count[i]++;
 			else
 				data_count[i] = 0;
