6bbd2f5ef838f4de9eb09f0795856f346db752b5
hyu nani <60097661+hyu-nani@users.noreply.github.com>
2021-12-21 10:58:33 +0900

    update ydlidar_node.cpp

--- src/ydlidar_ros/src/ydlidar_node.cpp
+++ src/ydlidar_ros/src/ydlidar_node.cpp
@@ -43,7 +43,7 @@ const int	printSize						=	100;
 //SSH print size 170 for laptop 100 for tablet
 int			pinMap[printSize][printSize]	=	{0};//SSH print map
 int			printScale						=	1;	//scale
-float		unitScale						=	5.0;//1-unit cm
+float		unitScale						=	10.0;//1-unit cm
 const int	allMapSize						=	2000;
 unsigned int	allMap[allMapSize][allMapSize] = {0};			//All map wall, sensing, robot
 unsigned int	allPointMap[allMapSize][allMapSize] = {0};		//score, departure point
