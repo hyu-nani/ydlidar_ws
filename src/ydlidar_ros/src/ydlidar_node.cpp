6ac8dc21f9de31feb0e44ee4104ae3d1515e583c
hyu nani <60097661+hyu-nani@users.noreply.github.com>
2021-12-21 10:46:15 +0900

    update ydlidar_node.cpp

--- src/ydlidar_ros/src/ydlidar_node.cpp
+++ src/ydlidar_ros/src/ydlidar_node.cpp
@@ -39,8 +39,8 @@ int		printCount = 0;
 int		lidarReadCount;
 
 
-const int	printSize						=	110;
-//SSH print size 170 for laptop 110 for tablet
+const int	printSize						=	100;
+//SSH print size 170 for laptop 100 for tablet
 int			pinMap[printSize][printSize]	=	{0};//SSH print map
 int			printScale						=	1;	//scale
 float		unitScale						=	5.0;//1-unit cm
