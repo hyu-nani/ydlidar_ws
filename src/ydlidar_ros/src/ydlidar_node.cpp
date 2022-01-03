6e77f6c26d7430c862de60d17966b649ea0004fe
hyu-nani <cube_h2@naver.com>
2022-01-03 15:48:12 +0900

    Update ydlidar_node.cpp

--- src/ydlidar_ros/src/ydlidar_node.cpp
+++ src/ydlidar_ros/src/ydlidar_node.cpp
@@ -536,9 +536,9 @@ void printSSHmonitor(int currentY,int currentX){
 		printf(" |");
 		for(int j=0;j<printSize;j++){
 			if(pinMap[i][j] == 1){        //sensing
-				printf("\033[33m");//orange
+				printf("\033[43m\033[33m");//orange
 				printf("::");
-				printf("\033[97m");//white
+				printf("\033[40m\033[97m");//white
 			}
 			else if(pinMap[i][j] == 2){   //hold , wall
 				printf("\033[41m");//red BG
