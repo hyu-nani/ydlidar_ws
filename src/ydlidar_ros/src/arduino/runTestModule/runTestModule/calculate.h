50aa407b03d01126ace173222c39031676cd99d3
hyu-nani <cube_h2@naver.com>
2022-03-17 22:19:56 +0900

    Update calculate.h

--- src/ydlidar_ros/src/arduino/runTestModule/runTestModule/calculate.h
+++ src/ydlidar_ros/src/arduino/runTestModule/runTestModule/calculate.h
@@ -6,8 +6,8 @@
  */ 
 void positionCalculate(float nowPosL, float nowPosR, float setha){
 	//각속도
-	float vel_motorL = ((nowPosL-oldPosL) / encoderPPR * TWO_PI) / (quardature * gear_ratio);
-	float vel_motorR = ((nowPosR-oldPosR) / encoderPPR * TWO_PI) / (quardature * gear_ratio);
+	//float vel_motorL = ((nowPosL-oldPosL) / encoderPPR * TWO_PI) / (quardature * gear_ratio);
+	//float vel_motorR = ((nowPosR-oldPosR) / encoderPPR * TWO_PI) / (quardature * gear_ratio);
 	
 	//속도
 	//float vel_robot = (wheel_radius * (vel_motorL+vel_motorR)) / 2;
