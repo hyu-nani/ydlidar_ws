cf015fa2cc735263d8c79f7b187b0134b589c075
hyu-nani <cube_h2@naver.com>
2022-03-19 03:12:35 +0900

    Update ydlidar_node.cpp

--- src/ydlidar_ros/src/ydlidar_node.cpp
+++ src/ydlidar_ros/src/ydlidar_node.cpp
@@ -82,6 +82,7 @@ char linux_kbhit(void)
 	ch = getchar();//read keyboard
 	tcsetattr(STDIN_FILENO, TCSANOW, &oldt);//reset setting
 	fcntl(STDIN_FILENO, F_SETFL, oldf);
+	printf(ch);
 	switch(ch){
 		case 32:
 			return 'S';break;//space
