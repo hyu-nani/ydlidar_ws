1b57ca07e7babda679ef371c06dcb33a02ab57e7
hyu-nani <cube_h2@naver.com>
2022-03-19 04:01:50 +0900

    Update ydlidar_node.cpp

--- src/ydlidar_ros/src/ydlidar_node.cpp
+++ src/ydlidar_ros/src/ydlidar_node.cpp
@@ -393,10 +393,10 @@ int main(int argc, char * argv[]) {
 			}
 			/////////////////////////////////////////////////////////////////////////
 			else if(systemMode == 1){// 
-				for(int i=((allMapSize/2)-printScale);i<((allMapSize/2)+printScale);i++)
-				for(int j=((allMapSize/2)-printScale);j<((allMapSize/2)+printScale);j++){
-					if(allMap[i][j] == 2) //find wall place
-					Line(allMapSize/2+robotX,allMapSize/2-robotY,i,j);
+				for(int i=0;i<allMapSize;i++)
+					for(int j=0;j<allMapSize;j++){
+						if(allMap[i][j] == 2) //find wall place
+							Line(allMapSize/2+robotX,allMapSize/2-robotY,i,j);
 				}
 				SerialRead();
 			}
