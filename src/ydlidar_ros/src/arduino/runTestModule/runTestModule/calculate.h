835c96602ec91b5033c92e7c155c432d76663bd0
hyu-nani <cube_h2@naver.com>
2022-03-19 17:25:07 +0900

    Update calculate.h

--- src/ydlidar_ros/src/arduino/runTestModule/runTestModule/calculate.h
+++ src/ydlidar_ros/src/arduino/runTestModule/runTestModule/calculate.h
@@ -5,12 +5,12 @@
  *  Author: cube_
  */ 
 void positionCalculate(double nowPosL, double nowPosR, double nowAngle){
+	double distanceL = diameter*PI/pulse*(nowPosL-oldPosL);
+	double distanceR = diameter*PI/pulse*(nowPosR-oldPosR);
 	//이동각도 계산
-	double c = ((nowPosL-oldPosL)-(nowPosR-oldPosR))/2;
+	double c = -(distanceL-distanceR)/2;
 	double angle = asin(2*sin(c/robot_wheel_pitch)*cos(c/robot_wheel_pitch));
 	//x, y 계산
-	double distanceL = diameter*PI/pulse*(abs(nowPosL-oldPosL));
-	double distanceR = diameter*PI/pulse*(abs(nowPosR-oldPosR));
 	robotX += cos(angle/2+nowAngle)*(distanceL+distanceR)/2;
 	robotY += -sin(angle/2+nowAngle)*(distanceL+distanceR)/2;
 	
