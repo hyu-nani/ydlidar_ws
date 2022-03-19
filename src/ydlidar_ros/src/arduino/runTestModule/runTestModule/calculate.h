57da02bcade8f1a9a4c738dd7ea497aa7646edc9
hyu-nani <cube_h2@naver.com>
2022-03-19 17:15:51 +0900

    Update calculate.h

--- src/ydlidar_ros/src/arduino/runTestModule/runTestModule/calculate.h
+++ src/ydlidar_ros/src/arduino/runTestModule/runTestModule/calculate.h
@@ -6,19 +6,20 @@
  */ 
 void positionCalculate(double nowPosL, double nowPosR, double nowAngle){
 	//이동각도 계산
-	double c = abs(((nowPosL-oldPosL)-(nowPosR-oldPosR))/2);
+	double c = ((nowPosL-oldPosL)-(nowPosR-oldPosR))/2;
 	double angle = 2*asin(c/robot_wheel_pitch);
 	//x, y 계산
 	double distanceL = diameter*PI/pulse*(abs(nowPosL-oldPosL));
 	double distanceR = diameter*PI/pulse*(abs(nowPosR-oldPosR));
-	robotX += cos(angle/2+robot_angle)*(distanceL+distanceR)/2;
-	robotY += -sin(angle/2+robot_angle)*(distanceL+distanceR)/2;
+	robotX += cos(angle/2+nowAngle)*(distanceL+distanceR)/2;
+	robotY += -sin(angle/2+nowAngle)*(distanceL+distanceR)/2;
 	
-	robot_angle += angle;
-	if(robot_angle > 180)
-		robot_angle = robot_angle - 360;
-	else if(robot_angle < -180)
-		robot_angle = robot_angle + 360;
+	nowAngle += angle;
+	if(nowAngle > 180)
+		nowAngle = nowAngle - 360;
+	else if(nowAngle < -180)
+		nowAngle = nowAngle + 360;
 	oldPosL = nowPosL;
 	oldPosR = nowPosR;
+	robot_angle = nowAngle;
 }
