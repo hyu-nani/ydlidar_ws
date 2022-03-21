afe83e98ec4134363bcd3ecf1ed5cfeae6b9c6f2
hyu-nani <cube_h2@naver.com>
2022-03-21 16:26:41 +0900

    Update ydlidar_node.cpp

--- src/ydlidar_ros/src/ydlidar_node.cpp
+++ src/ydlidar_ros/src/ydlidar_node.cpp
@@ -737,7 +737,7 @@ void printSSHmonitor(int currentY,int currentX){
 				printf("  ");
 				prePixel = 0;
 			}
-			usleep(10);
+			usleep(1);
 		}
 		printf("\033[40m\033[97m");//black BG & white
 		printf("|\n");
