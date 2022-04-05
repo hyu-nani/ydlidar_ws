e29160f07fb69c9ce23b47cc618ee6980ff5c1ea
hyu-nani <cube_h2@naver.com>
2022-04-05 17:59:26 +0900

    Update hardware.h

--- src/ydlidar_ros/src/arduino/runTestModule/runTestModule/hardware.h
+++ src/ydlidar_ros/src/arduino/runTestModule/runTestModule/hardware.h
@@ -19,7 +19,7 @@
 //#define gear_ratio			100
 //#define gear		0.2
 //#define volume		0.25
-#define diameter	13.0
+#define diameter	12.5
 #define pulse		6400
 //#define radius		6
 //#define encoderPPR	28
