eff4b44f1fa1ef76e22ddcce500309bf0b343f4f
HyeonHan Bae <60097661+hyu-nani@users.noreply.github.com>
2022-04-11 16:28:07 +0900

    update calculate.h

--- src/ydlidar_ros/src/arduino/runTestModule/runTestModule/calculate.h
+++ src/ydlidar_ros/src/arduino/runTestModule/runTestModule/calculate.h
@@ -1,7 +1,7 @@
 /*
  * calculate.h
  *
- * Created: 2022-03-16 ¿ÀÈÄ 9:26:56
+ * Created: 2022-03-16 ï¿½ï¿½ï¿½ï¿½ 9:26:56
  *  Author: cube_
  */ 
 void positionCalculate(double nowPosL, double nowPosR){
@@ -10,12 +10,12 @@ void positionCalculate(double nowPosL, double nowPosR){
 	oldPosL = nowPosL;
 	oldPosR = nowPosR;
 	
-	//ÀÌµ¿°¢µµ °è»ê
+	//ï¿½Ìµï¿½ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½
 	double c = (distanceL-distanceR)/2;
 	//double angle = asin(2*sin(c/robot_wheel_pitch)*cos(c/robot_wheel_pitch))*(PI/180);
 	double angle = -2*asin(c/robot_wheel_pitch)*(180.0/M_PI);
 
-	//x, y °è»ê
+	//x, y ï¿½ï¿½ï¿½
 	double distance = (distanceL+distanceR)/2;
 	robotX += -sin(((angle/2+robot_angle))*(M_PI/180.0))*distance;
 	robotY += cos(((angle/2+robot_angle))*(M_PI/180.0))*distance;
@@ -28,20 +28,20 @@ void positionCalculate(double nowPosL, double nowPosR){
 }
 
 //0:stop, 1:front, 2:left, 3:right, 4:back
-void errorGapCal(double nowAngle, double nowPosL, double nowPosR, int direct){
+void errorGapCal(double nowPosL, double nowPosR, int direct){
 	double distanceL1 = diameter*M_PI/pulse*(nowPosL-oldPosL1);
 	double distanceR1 = diameter*M_PI/pulse*(nowPosR-oldPosR1);
 	oldPosL1 = nowPosL;
 	oldPosR1 = nowPosR;
 	int val = 10;
-	//°³º° ¹æÇâ ¼Óµµ Á¦¾î
+	//ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ ï¿½Óµï¿½ ï¿½ï¿½ï¿½ï¿½
 	if(direct==0){//stop
 		errorGap = 0;
 	}
 	else if(direct==1){//front
-		if(fixAngle < nowAngle)//rising
+		if(distanceL1>distanceR1)//rising
 			errorGap = +val;
-		else if(fixAngle > nowAngle)
+		else if(distanceL1<distanceR1)
 			errorGap = -val;
 		else
 			errorGap = 0;
@@ -63,12 +63,11 @@ void errorGapCal(double nowAngle, double nowPosL, double nowPosR, int direct){
 			errorGap = 0;
 	}
 	else if(direct==4){//back
-		if(fixAngle < nowAngle)//rising
+		if(distanceL1<distanceR1)//rising
 			errorGap = -val;
-		else if(fixAngle > nowAngle)
+		else if(distanceL1>distanceR1)
 			errorGap = +val;
 		else
 			errorGap = 0;
 	}
-	
 }
