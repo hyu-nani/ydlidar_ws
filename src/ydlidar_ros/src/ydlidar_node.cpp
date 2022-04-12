18acbcf1ff362e3c91f47ef724529b4b3bdb51b7
HyeonHan Bae <60097661+hyu-nani@users.noreply.github.com>
2022-04-12 16:06:18 +0900

    update ydlidar_node.cpp

--- src/ydlidar_ros/src/ydlidar_node.cpp
+++ src/ydlidar_ros/src/ydlidar_node.cpp
@@ -49,13 +49,13 @@ float val = 0;
 
 int	systemMode	=	1;	// 0 = default, 1 = control robot mode
 
-const int	printSize								=	50;//185
+const int	printSize								=	150;//185
 //SSH print size 170 for laptop 100 for tablet
 
 int			pinMap[printSize][printSize]			=	{0};	//SSH print map
 int			printScale								=	1;		//scale
 float		unitScale								=	10;		//1-unit cm
-const int	allMapSize								=	100;		//maximum 65535
+const int	allMapSize								=	2000;		//maximum 65535
 unsigned int	allMap[allMapSize][allMapSize]      =	{0};		//All map wall, sensing, robot
 unsigned int	allPointMap[allMapSize][allMapSize] =	{0};		//score, departure point
 unsigned int	oldMap[allMapSize][allMapSize]		=	{0};		//short-term map
