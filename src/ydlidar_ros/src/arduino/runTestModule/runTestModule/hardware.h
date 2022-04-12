7ee27672c971599d3bf022309e31245e77ef01a6
HyeonHan Bae <60097661+hyu-nani@users.noreply.github.com>
2022-04-12 17:23:49 +0900

    update hardware.h

--- src/ydlidar_ros/src/arduino/runTestModule/runTestModule/hardware.h
+++ src/ydlidar_ros/src/arduino/runTestModule/runTestModule/hardware.h
@@ -15,7 +15,7 @@
 
 //information of robo
 //#define wheel_radius		6
-float	robot_wheel_pitch	=	40.45; 
+float	robot_wheel_pitch	=	48.65; 
 //#define gear_ratio			100
 //#define gear		0.2
 //#define volume		0.25
