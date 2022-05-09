b1000a0ef40ec355ea94ec57ef18991d2d4d96ab
hyu-nani <cube_h2@naver.com>
2022-05-09 14:02:42 +0900

    Update hardware.h

--- src/ydlidar_ros/src/arduino/runTestModule/runTestModule/hardware.h
+++ src/ydlidar_ros/src/arduino/runTestModule/runTestModule/hardware.h
@@ -15,7 +15,7 @@
 
 //information of robo
 //#define wheel_radius		6//
-float	robot_wheel_pitch	=	39.8;//distance
+float	robot_wheel_pitch	=	40.5;//distance
 //#define gear_ratio			100
 //#define gear		0.2
 //#define volume		0.25
