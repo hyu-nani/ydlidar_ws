2c7222a235d065b33164610afa4ab0f837516af4
hyu-nani <cube_h2@naver.com>
2022-05-23 00:00:35 +0900

    업데이트

--- src/ydlidar_ros/src/pathWayFind.cpp
+++ src/ydlidar_ros/src/pathWayFind.cpp
@@ -16,9 +16,10 @@
 	즉각적인 전방 회피를 위해 순간적으로 센싱되는 곳 또한 벽으로 간주한다.
 */
 unsigned int	mazeMap[allMapSize][allMapSize]	=	{0};
+unsigned int	countMaze[allMapSize][allMapSize]	=	{0};
 int		startX, StartY;
 int		endX, endY;
-
+int		moveX = 0, moveY = 0;
 /*
 	Convert allmap to mazeMap
 	and
@@ -31,21 +32,87 @@ void initMap(){
 	StartY	=	robotY;
 	endX	=	departureX;
 	endY	=	departureY;
-	for(int y=0;y<allMapSize;y++)
-	for(int x=0;x<allMapSize;x++){
+	for(int y = startX; y < allMapSize; y++)
+	for(int x = 0; x < allMapSize; x++){
 		int dot = allMap[y][x];
 		if(	dot == 0 ||	//none
 			dot == 1 ||	//sensing
 			dot == 2)	//wall
-			mazeMap[y][x] = 0;//wall
+			mazeMap[y][x] = 0;	//wall
 		else
-			mazeMap[y][x] = 1;//path
+			mazeMap[y][x] = 1;	//path
 	}
 }
 
 /*
 	Find path way
+	sx = start X
+	sy = start Y
+	ex = departure X
+	ey = departure Y
 */
-void findWay(){
-	
+void findWay(int sx, int sy, int ex, int ey){
+	int count = 1;
+	countMaze[sy][sx] = 1;
+	//counting maze
+	while(countMaze[ey][ex] != count){
+		for (int y = -count + sy; y < count + sy; y++)
+		for (int x = -count + sx; x < count + sx; x++) {
+			if (x < 0 || y < 0 || x > allMapSize || y > allMapSize)//range over
+			continue;
+			if (countMaze[y][x] == count) {//check way
+				if (mazeMap[y + 1][x] == 1 && countMaze[y + 1][x] == 0)
+					countMaze[y + 1][x] = count + 1;
+				if (mazeMap[y - 1][x] == 1 && countMaze[y - 1][x] == 0)
+					countMaze[y - 1][x] = count + 1;
+				if (mazeMap[y][x + 1] == 1 && countMaze[y][x + 1] == 0)
+					countMaze[y][y + 1] = count + 1;
+				if (mazeMap[y][x - 1] == 1 && countMaze[y][x - 1] == 0)
+					countMaze[y][x - 1] = count + 1;
+			}
+		}
+		count++;
+	}
+	//find way inversion counting
+	int nowX = ex, nowY = ey;
+	for (int i = count; i > 0; i--) {
+		if (countMaze[nowX][nowY] = i)
+		countMaze[nowX][nowY] = -1;
+		if (i - 1 == countMaze[nowX - 1][nowY]) {
+			countMaze[nowX - 1][nowY] = -1;
+			nowX--;
+		}
+		else if (i - 1 == countMaze[nowX + 1][nowY]) {
+			countMaze[nowX + 1][nowY] = -1;
+			nowX++;
+		}
+		else if (i - 1 == countMaze[nowX][nowY - 1]) {
+			countMaze[nowX][nowY - 1] = -1;
+			nowY--;
+		}
+		else if (i - 1 == countMaze[nowX][nowY + 1]) {
+			countMaze[nowX][nowY + 1] = -1;
+			nowY++;
+		}
+	}
+	printf("%d", count);
+	//next move point
+	moveX = 0;
+	moveY = 0;
+	if(countMaze[sy - 1][sx] == -1){
+		moveX = sx;
+		moveY = sy - 1;
+	}
+	else if(countMaze[sy + 1][sx] == -1){
+		moveX = sx;
+		moveY = sy + 1;
+	}
+	else if(countMaze[sy][sx - 1] == -1){
+		moveX = sx - 1;
+		moveY = sy;
+	}
+	else if(countMaze[sy][sx + 1] == -1){
+		moveX = sx + 1;
+		moveY = sy;
+	}
 }
