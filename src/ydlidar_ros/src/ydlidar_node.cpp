d9d6bdfc96980ffc3f35e57fefc8731e89033f99
hyu-nani <cube_h2@naver.com>
2022-07-06 15:52:19 +0900

    Update ydlidar_node.cpp

--- src/ydlidar_ros/src/ydlidar_node.cpp
+++ src/ydlidar_ros/src/ydlidar_node.cpp
@@ -374,8 +374,8 @@ int main(int argc, char * argv[]) {
 				if(data_count[i] > 4 && mappingActive == true && ignoreTime == 0){//wall sensitivity
 					if((allMapSize/2-robotY+Yvalue)>0&&(allMapSize/2-robotY+Yvalue)<allMapSize)
 						if((allMapSize/2+robotX+Xvalue)>0&&(allMapSize/2+robotX+Xvalue)<allMapSize){
-							allMap[allMapSize/2-robotY+Yvalue][allMapSize/2+robotX+Xvalue] = 2; //hold
 							Line(allMapSize/2-robotY,allMapSize/2+robotX,allMapSize/2-robotY+Yvalue,allMapSize/2+robotX+Xvalue);
+							allMap[allMapSize/2-robotY+Yvalue][allMapSize/2+robotX+Xvalue] = 2; //hold
 						}
 					data_count[i] = 0;
 				} 
