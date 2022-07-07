31276a7bd6f9cf3d37540055c7ce8bf0f1e41b3c
hyu-nani <cube_h2@naver.com>
2022-07-07 15:50:28 +0900

    Update pathWayFind.cpp

--- src/ydlidar_ros/src/pathWayFind.cpp
+++ src/ydlidar_ros/src/pathWayFind.cpp
@@ -47,16 +47,19 @@ void initMap(){
 		countMaze[y][x] = 0;
 	}
 	for(int y = 1; y < allMapSize-1; y++)
-	for(int x = 1; x < allMapSize-1; x++){
-		if( mazeMap[y][x] == 0 )
-		if( mazeMap[y+1][x] == 1 || 
-			mazeMap[y][x+1] == 1 || 
-			mazeMap[y-1][x] == 1 || 
-			mazeMap[y][x-1] == 1)
-		for(int i=0;i<3;i++)
-		for(int j=0;j<3;j++)
-		mazeMap[y+i-1][x+j-1] = 0;
-	}
+	for(int x = 1; x < allMapSize-1; x++)
+	if( mazeMap[y][x] == 0 )
+	if( mazeMap[y+1][x] == 1 ||
+	mazeMap[y][x+1] == 1 ||
+	mazeMap[y-1][x] == 1 ||
+	mazeMap[y][x-1] == 1)
+	mazeMap[y+i-1][x+j-1] = 2;
+	for(int y = 1; y < allMapSize-1; y++)
+	for(int x = 1; x < allMapSize-1; x++)
+	if( mazeMap[y][x] == 2 )
+	for(int i=0;i<3;i++)
+	for(int j=0;j<3;j++)
+	mazeMap[y+i-1][x+j-1] = 0;
 }
 bool findWay(int sx, int sy, int ex, int ey){
 	int count = 1;
@@ -111,8 +114,6 @@ bool findWay(int sx, int sy, int ex, int ey){
 			allMap[nowY][nowX + 1] = 6;
 			nowX++;
 		}
-		else{i++;}
-		
 	}
 	//printf("%d\n", count);
 	//next move point
