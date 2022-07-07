329e43107b95990b716f91d55bf29e8f3d066e78
hyu-nani <cube_h2@naver.com>
2022-07-07 15:25:27 +0900

    Update pathWayFind.cpp

--- src/ydlidar_ros/src/pathWayFind.cpp
+++ src/ydlidar_ros/src/pathWayFind.cpp
@@ -46,6 +46,18 @@ void initMap(){
 		}
 		countMaze[y][x] = 0;
 	}
+	for(int y = 0; y < allMapSize; y++)
+	for(int x = 0; x < allMapSize; x++){
+		int dot = mazeMap[y][x];
+		if( dot == 0 )
+		if( mazeMap[y+1][x] == 1 ||
+			mazeMap[y][x+1] == 1 ||
+			mazeMap[y-1][x] == 1 ||
+			mazeMap[y][x-1] == 1 ||)
+		for(int i=-1;i<2;i++)
+		for(int j=-1;j<2;j++)
+		mazeMap[y+i][x+j] = 0;
+	}
 }
 bool findWay(int sx, int sy, int ex, int ey){
 	int count = 1;
