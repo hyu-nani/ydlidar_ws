30fb906af5c4775ecb3791d7516e4f7c4e3dcc36
hyu-nani <cube_h2@naver.com>
2022-07-14 15:19:16 +0900

    Update ydlidar_node.cpp

--- src/ydlidar_ros/src/ydlidar_node.cpp
+++ src/ydlidar_ros/src/ydlidar_node.cpp
@@ -705,27 +705,6 @@ int main(int argc, char * argv[]) {
 				}
 				loadfile.close();
 				printf("loading......");
-			}else if(kb.compare("f")==0){
-				int filterPoint=0;
-				for(int i=1;i<allMapSize;i++)
-					for(int j=1;j<allMapSize;j++)
-						if(allMap[i][j] == 0){
-							for(int k=0;k<3;k++)
-								for(int p=0;p<3;p++)
-									if(allMap[i-1+k][j-1+p]==4)
-										filterPoint++;
-								if(filterPoint>4)
-									allMap[i][j] = 4;
-								filterPoint = 0;
-								for(int k=0;k<3;k++)
-								for(int p=0;p<3;p++)
-								if(allMap[i-1+k][j-1+p]==2)
-								filterPoint++;
-								if(filterPoint>4)
-								allMap[i][j] = 2;
-								filterPoint = 0;
-						}
-			}
 			}else if(kb.compare("DC3")==0){//Ctrl-S
 				ofstream savefile;
 				savefile.open("map.txt");
